29b0eba3215ab86992968eac7e5fe32c4e595057
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-02-06 15:15:09 +0100

    Code mort

--- src/HR-Helpers/AccessData/SqlContextAccess.cs
+++ src/HR-Helpers/AccessData/SqlContextAccess.cs
@@ -481,49 +481,10 @@ namespace AccessData
                     + $"AND ColonneId={valeur.IdColonne};";
 
                 await ExecuteCoreAsync(cmdUpdate);
-
-
-                var tt = @"CREATE TABLE Tableau (IdTableau VARCHAR(255) NOT NULL,
-IdUser VARCHAR(255) NOT NULL,
-NomTableau VARCHAR(250) NOT NULL,
-DescriptionTable VARCHAR(250),
-DateFinInscription DATE NOT NULL,
-PRIMARY KEY(IdTableau, IdUser));
-
-CREATE TABLE Colonne
-(IdColonne INT NOT NULL,
-TableId VARCHAR(255) NOT NULL,
-NomColonne VARCHAR(250) NOT NULL,
-DescriptionColonne VARCHAR(250),
-TypeData VARCHAR(30),
-PRIMARY KEY(IdColonne, TableId),
-FOREIGN KEY(TableId) REFERENCES Tableau(IdTableau) ON DELETE CASCADE);
-
-CREATE TABLE Valeur
-(NumeroLigne INT NOT NULL,
-ColonneId INT NOT NULL,
-TableId VARCHAR(255) NOT NULL,
-UserId VARCHAR(255) NOT NULL,
-Valeur VARCHAR(255) NOT NULL,
-PRIMARY KEY(NumeroLigne, ColonneId, TableId, UserId),
-FOREIGN KEY(TableId) REFERENCES Tableau(IdTableau) ON DELETE CASCADE,
-FOREIGN KEY(ColonneId) REFERENCES Colonne(IdColonne) ON DELETE CASCADE);";
-
             }
         }
 
         #endregion
-
-        #region Colonne
-
-
-
-
-        #endregion
-
-
-
-
 
         #region Private Methods
 
